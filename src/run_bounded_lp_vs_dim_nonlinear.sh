b94ccaf98d36b2f5f1b46cca9e68854bc6756334
Lucia Pezzetti <lucia.pezzetti21@gmail.com>
2026-02-24 17:52:36 +0100

    refactor: clean up

--- src/run_bounded_lp_vs_dim_nonlinear.sh
+++ src/run_bounded_lp_vs_dim_nonlinear.sh
@@ -3,7 +3,7 @@
 # Script to run bounded_lp_vs_dim_nonlinear.py with different N values
 
 # Array of N values to test
-N_VALUES=(5000)
+N_VALUES=(500 1000 1500 2000 2500 3000 3500 4000 4500 5000 10000 15000 20000)
 
 # Change to the directory where the script is located (src directory)
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
